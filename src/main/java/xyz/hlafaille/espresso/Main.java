a55c5955f620d1c7aea901fc5cb649da8ae05d0d
Hunter LaFaille <hlafaille@proton.me>
2023-11-12 19:05:09 -0500

    hey look, gson works!

--- src/main/java/xyz/hlafaille/espresso/Main.java
+++ src/main/java/xyz/hlafaille/espresso/Main.java
@@ -1,7 +1,21 @@
 package xyz.hlafaille.espresso;
 
+import com.google.gson.Gson;
+import xyz.hlafaille.espresso.configuration.dto.ConfigurationFileDto;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Gson gson = new Gson();
+        try (BufferedReader reader = new BufferedReader(new FileReader("example/example.espresso.json"))) {
+            ConfigurationFileDto configurationFileDto = gson.fromJson(reader, ConfigurationFileDto.class);
+            System.out.println(configurationFileDto);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
