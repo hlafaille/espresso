208957cb850d564e41d2dc161b8f9a1946c211d6
Hunter LaFaille <hlafaille@proton.me>
2023-11-12 21:52:00 -0500

    resolving to the proper directory now

--- src/main/java/xyz/hlafaille/espresso/Main.java
+++ src/main/java/xyz/hlafaille/espresso/Main.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
 /**
  * This will eventually be replaced with a proper CLI. It doesn't make sense to develop the CLI and different modules
  * together, as they will inevitably be too close together for their own good. Developing them largely separately
- * will allow this project to come together using building block style of development.
+ * will allow this project to come together using building block style development.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
