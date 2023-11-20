b251ecd630692a82921856dfe73d900e6c29234a
Hunter LaFaille <hlafaille@proton.me>
2023-11-20 16:52:33 -0500

    implementing eap

--- src/main/java/xyz/hlafaille/espresso/Main.java
+++ src/main/java/xyz/hlafaille/espresso/Main.java
@@ -1,6 +1,8 @@
 package xyz.hlafaille.espresso;
 
 
+import xyz.hlafaille.eap.EspressoArgumentParser;
+import xyz.hlafaille.eap.exception.*;
 import xyz.hlafaille.espresso.cli.ArgumentParser;
 import xyz.hlafaille.espresso.cli.Command;
 import xyz.hlafaille.espresso.cli.handler.InitCommandHandler;
@@ -14,44 +16,13 @@ import java.io.IOException;
 import java.util.logging.Logger;
 
 public class Main {
-    public static void main(String[] args) {
-        // build our argument parser
-        ArgumentParser argumentParser = new ArgumentParser(
+    public static void main(String[] args) throws EapCommandModifierNotFoundException, EapCommandNotFoundException,
+            EapSubcommandNotFoundException, EapCommandNotSpecifiedException, EapMalformedCommandModifierException,
+            EapMissingSubcommandException {
+        EspressoArgumentParser espressoArgumentParser = new EspressoArgumentParser(
                 "Espresso",
-                "An opinionated build tool for Java. Because Maven and Gradle don't have opinions."
+                "Wake up your workflow with Espresso"
         );
-
-        // espresso initialize
-        argumentParser.addCommand(
-                new Command(
-                        "initialize",
-                        "init",
-                        "Establish a new Espresso project"
-                ),
-                new InitCommandHandler()
-        );
-
-        // espresso pull
-        argumentParser.addCommand(
-                new Command(
-                        "pull",
-                        "p",
-                        "Pull dependencies regardless of local status"
-                ),
-                new PullCommandHandler()
-        );
-
-        // espresso install
-        argumentParser.addCommand(
-                new Command(
-                        "install",
-                        "i",
-                        "Install a dependency"
-                ),
-                new InstallCommandHandler()
-        );
-
-        // parse the arguments
-        argumentParser.parse(args);
+        espressoArgumentParser.parse(args);
     }
 }
